cc7216f6dd2abd2ea70fa4a09cbe52b11d4e27e4
Trương Nguyễn Công Chính <88539197+ninehcobra@users.noreply.github.com>
2024-10-11 13:44:37 +0700

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,7 +32,7 @@ export default function RootLayout({
           rel='stylesheet'
           href='https://site-assets.fontawesome.com/releases/v6.1.2/css/all.css?fbclid=IwAR2undhfna4tMQt0yyPEeuUmtLt5QD9hl2TxmpC3H3oo--fAJX6skSyYftg%22%3E'
         />
-       <script src="https://chinhtruong2903.github.io/test-widget-chat/bundle.js?token=9071c599-cdd1-4f3b-9b02-cf2592392f4a"></script>
+       <script src="https://chinhtruong2903.github.io/test-widget-chat/bundle.js?token=cfcb9df8-f55c-4717-bbab-7b2d94883fc8"></script>
       </head>
       <body className={'vh-100'}>
         <ToastContainer
